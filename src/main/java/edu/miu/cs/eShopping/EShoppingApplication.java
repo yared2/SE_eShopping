b756b8bb298c2b01fbbeca0384beb2fa2d9a4109
yared2 <yared555@gmail.com>
2020-03-07 16:35:37 -0600

    swagger fix

--- src/main/java/edu/miu/cs/eShopping/EShoppingApplication.java
+++ src/main/java/edu/miu/cs/eShopping/EShoppingApplication.java
@@ -24,7 +24,7 @@ public class EShoppingApplication {
 	    public Docket api() { 
 	        return new Docket(DocumentationType.SWAGGER_2)  
 	          .select()  
-	          .apis(RequestHandlerSelectors.basePackage("miu.edu"))             
+	          .apis(RequestHandlerSelectors.basePackage("edu.miu.cs.eShopping"))             
 	          .paths(PathSelectors.any())                          
 	          .build()
 	          .apiInfo(apiInfo());                                           
